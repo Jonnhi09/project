8d13448cf6a69cd9b882286e49b1e8f05487bdee
Sergio Rodriguez <sergio200035@gmail.com>
2018-02-19 23:21:03 -0500

    Autor 1 pruebas registar cliente.

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -5,7 +5,10 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquilerItemsStub;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -22,8 +25,31 @@ public class ClientesTest {
     @Before
     public void setUp() {
     }
+    /**
+     * Clases de quivalencia, resultado
+     * el cliente ya esta registrado, no se registra de nuevo ni se o se pierde unfomacion debe arrojar la excepcion sin modificar.
+     * El clinete es nuevo, se debe hacer el registro del cliente.
+     * @throws ExcepcionServiciosAlquiler 
+     */
+    @Test
+    public void registrarCliente() throws ExcepcionServiciosAlquiler{
+        Cliente p = new Cliente();
+        ServiciosAlquilerItemsStub serv = new ServiciosAlquilerItemsStub();
+        serv.registrarCliente(p);
+        List<Cliente> lc = serv.consultarClientes();
+        boolean b = false;
+        try{
+            serv.registrarCliente(p);
+        }catch(ExcepcionServiciosAlquiler e){
+            b = true;
+        }
+        if(!serv.consultarClientes().equals(lc) || b){
+            fail("se modificaron datos o no se envia la excepcion adecuada, fallo el caso");
+        }
+        
+    }
     
-  
+    
     @Test
     public void additems1() throws ExcepcionServiciosAlquiler{
     	
