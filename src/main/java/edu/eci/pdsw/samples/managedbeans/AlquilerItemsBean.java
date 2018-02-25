12173b98d69f3a5641f33d7e1a17c7ef649014e9
Sergio Rodriguez <sergio200035@gmail.com>
2018-02-25 18:25:43 -0500

    RegistroCliente

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,10 +5,17 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -19,8 +26,78 @@ import javax.faces.bean.SessionScoped;
 public class AlquilerItemsBean implements Serializable {
 
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
+    private Cliente selected;
+    private String direccion;
+    private long documento;
+    private String email;
+    private String nombre;
+    private String telefono;
 
     public AlquilerItemsBean() {
     }
+    
+    public List<Cliente> getClientes() throws ExcepcionServiciosAlquiler {
+        return sp.consultarClientes();
+    }
+    
+    public void registrarCliente(){
+        try {
+            sp.registrarCliente(new Cliente(nombre, documento, telefono, direccion, email));
+        } catch (ExcepcionServiciosAlquiler ex) {
+            Logger.getLogger(AlquilerItemsBean.class.getName()).log(Level.SEVERE, null, ex);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Usuario ya registrado",ex.getMessage()));
+        }
+    }
+
+    public Cliente getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Cliente selected) {
+        this.selected = selected;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public long getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(long documento) {
+        this.documento = documento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
 
 }
